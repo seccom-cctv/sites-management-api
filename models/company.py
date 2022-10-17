e656bc7f5904305586839d473f58d1484143be46
zepedrotrigo <josetrigo@ua.pt>
2022-10-17 07:54:25 +0100

    created database models

---
+++ models/company.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from config.database import Base
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), unique=False, index=True)
+    address = Column(String(255), unique=True, index=False)
+    phone = Column(String(30), unique=True, index=False)
+    email = Column(String(255), unique=True, index=False)
+
+    buildings = relationship("Building", back_populates="company")
+    administrators = relationship("Administrator", back_populates="company")
